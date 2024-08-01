63c8e5f26e2987e51246a14da4473fac4ebf987b
Staicy254 <jeanstaicy21@gmail.com>
2024-08-01 16:37:23 +0300

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """A python module that determines if all boxes can be opened
    from a list of lists
 """
 
 
 def canUnlockAll(boxes=[]):
-    """A function that returns True if all boxes in
-    the list can be opened
+    """A function that returns True of all box in
+    boxes can be opend
     """
     if not boxes:
         return False
 
-    keys = set([0])  # Start with the key to the first box
+    keys = set([0])
     for box_id, box in enumerate(boxes):
         for key in box:
             if key < len(boxes) and key != box_id:
@@ -30,13 +30,13 @@ if __name__ == '__main__':
                 [3, 0],
                 [1, 2, 3],
             ]
-    print(canUnlockAll(boxes))  # Call the correct function
+    print(unlockBoxes(boxes))
 
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Call the correct function
+    print(canUnlockAll(boxes))
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Call the correct function
+    print(unlockBoxes(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Call the correct function
+    print(unlockBoxes(boxes))
