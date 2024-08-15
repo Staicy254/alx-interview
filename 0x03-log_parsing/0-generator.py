2b4d19fb44709cf521b2215a136992df69c499ef
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2024-08-15 21:07:40 +0300

    log-parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,15 +6,10 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    ip_part = "{:d}.{:d}.{:d}.{:d}".format(
-        random.randint(1, 255), random.randint(1, 255),
-        random.randint(1, 255), random.randint(1, 255)
-    )
-    timestamp = datetime.datetime.now()
-    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
-    response_time = random.randint(1, 1024)
-    sys.stdout.write(
-        f"{ip_part} - [{timestamp}] \"GET /projects/260 HTTP/1.1\" "
-        f"{status_code} {response_time}\n"
-    )
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
     sys.stdout.flush()
